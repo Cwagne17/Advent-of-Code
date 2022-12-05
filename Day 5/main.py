7c78b4923a0b8eb938d0597f357b6ecc3102dd78
Cwagne17 <cwagne17@students.towson.edu>
2022-12-05 09:58:23 -0500

    add the drawing docs

--- Day 5/main.py
+++ Day 5/main.py
@@ -1,3 +1,15 @@
+"""stack_dict represents the following drawing.
+
+[B]                     [N]     [H]
+[V]         [P] [T]     [V]     [P]
+[W]     [C] [T] [S]     [H]     [N]
+[T]     [J] [Z] [M] [N] [F]     [L]
+[Q]     [W] [N] [J] [T] [Q] [R] [B]
+[N] [B] [Q] [R] [V] [F] [D] [F] [M]
+[H] [W] [S] [J] [P] [W] [L] [P] [S]
+[D] [D] [T] [F] [G] [B] [B] [H] [Z]
+ 1   2   3   4   5   6   7   8   9 
+"""
 stack_dict = {
     "1": ["D", "H", "N", "Q", "T", "W", "V", "B"],
     "2": ["D", "W", "B"],
