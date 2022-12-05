539522a2ba0e9e0528a8a487b07a7bb75d6ac659
Cwagne17 <cwagne17@students.towson.edu>
2022-12-05 09:57:22 -0500

    Day 5 comments added

--- Day 5/main.py
+++ Day 5/main.py
@@ -10,36 +10,45 @@ stack_dict = {
     "9": ["Z", "S", "M", "B", "L", "N", "P", "H"],
 }
 
-def crate_mover_9000(count: int, source: str, dest: str):
-    for i in range(count):
+def crate_mover_9000(n: int, source: str, dest: str):
+    """Procedure that represents the crane moving one crate at a time from the source to the destination 'n' times.
+
+    Args:
+        n (int): The number of times the crane should move a crate from the source stack.
+        source (str): The source stack of crates the crane should move from.
+        dest (str): The destination stack of crates the crane should move to.
+    """
+    for i in range(n):
         crate = stack_dict[source].pop()
         stack_dict[dest].append(crate)
 
-def crate_mover_9001(count: int, source: str, dest: str):
-    crates = [stack_dict[source].pop() for i in range(count)]
+def crate_mover_9001(n: int, source: str, dest: str):
+    """Procedure that represents the crane moving 'n' crates from the source stack to the destination stack.
+
+    Args:
+        n (int): The number of crates the crane should move at once.
+        source (str): The source stack of crates the crane should move from.
+        dest (str): The destination stack of crates the crane should move to.
+    """
+    crates = [stack_dict[source].pop() for i in range(n)]
     for crate in reversed(crates):
         stack_dict[dest].append(crate)
 
 def print_top_crates():
+    """Prints the last value (top) of each stack in order 1-9.
+    """
     for value in stack_dict.values():
         print(value[-1], end="")
+    print()
 
 with open('input.txt') as fin:
-    data = [ln.replace('\n', '') if ln != '\n' else ln for ln in fin.readlines()]
-    procedures = data[data.index('\n')+1:]
+    data = [ln.replace('\n', '') if ln != '\n' else ln for ln in fin.readlines()] # Reads in file removing \n from the end of lines, ignoring empty newlines
+    procedures = data[data.index('\n')+1:] # Gets all procedures (after drawing)
     for procedure in procedures:
-        steps = procedure.split()
+        steps = procedure.split() # format 'move n from source to dest'
+        
+        # Because the stack_dict data structure is manipulated by both functions only
+        # one function can be used at a given time.
         # crate_mover_9000(int(steps[1]), steps[3], steps[5])
         crate_mover_9001(int(steps[1]), steps[3], steps[5])
     print_top_crates()
-    print()
-    
-    
-    
-    
-    
-    
-    
-        
-        
-            
