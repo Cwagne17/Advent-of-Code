1940133c43931604275f99089daa3464e51c3000
Christohper Wagner <cwagne17@students.towson.edu>
2023-12-01 21:26:27 -0500

    part 1 & 2 done

--- 2023/Day_1/main.py
+++ 2023/Day_1/main.py
@@ -1,47 +1,62 @@
-input = """1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
-"""
+import re
 
-input_sum = 0
-with open("input.txt") as fin:
-    input = fin.read()
+
+def part_one(input):
+    sum = 0
+
     for line in input.splitlines():
-        calibration_value = "".join([char for char in line if not char.isalpha()])
-        input_sum += int(f"{calibration_value[0]}{calibration_value[-1]}")
-print("Part 1: ", input_sum)
+        filtered_line = "".join([char for char in line if char.isdigit()])
+        sum += int(f"{filtered_line[0]}{filtered_line[-1]}")
 
-input_sum = 0
+    return sum
+
+
+def part_two(input):
+    # Digits as strings
+    digits = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ]
+
+    new_input = []
+
+    for line in input.splitlines():
+        ids = {}
+        for value, digit in enumerate(
+            digits[1:], 1
+        ):  # Skip the empty string at index 0
+            if digit in line:
+                # Find all indices of the digit word
+                indices = [m.start() for m in re.finditer(digit, line)]
+                for index in indices:
+                    ids[index] = str(value)
+
+        num = ""
+        for i, c in enumerate(line):
+            if i in ids:
+                num += ids[i]
+                continue
+            if c.isdigit():
+                num += c
+
+        new_input.append(num)
+
+    sum_result = part_one("\n".join(new_input))
+    return sum_result
+
+
 digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-
-def replace_digit_with_position(line, digit, position):
-    return line.replace(digit, str(position))
-
-
 with open("input.txt") as fin:
-    for line in fin.read().splitlines():
-        # Replace digits with words
-        for char in line:
-            if char.isdigit():
-                line = line.replace(char, digits[int(char) - 1])
+    input = fin.read()
 
-        line_indexes = []
-        for digit in digits:
-            try:
-                for i in range(len(line)):
-                    line_indexes.append((line.index(digit), digit))
-            except ValueError:
-                pass
-        line_indexes.sort()
-
-        first = line_indexes[0]
-        last = line_indexes[-1]
-
-        first_value = digits.index(first[1]) + 1
-        last_value = digits.index(last[1]) + 1
-        calibration_value = f"{first_value}{last_value}"
-        input_sum += int(calibration_value)
-
-print("Part 2: ", input_sum)
+    print("Part 1: ", part_one(input))
+    print("Part 2: ", part_two(input))
