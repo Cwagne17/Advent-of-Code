6716eab14029147e908d3bc4cc90ec5245447438
chris wagner <cwagne17@students.towson.edu>
2023-12-03 20:45:43 -0500

    day 3 finished finally, thanks josue

--- 2023/Day_3/main.py
+++ 2023/Day_3/main.py
@@ -5,33 +5,49 @@ symbols = set()
 
 seen = set()
 
+
 with open("input.txt") as fin:
     for i, row in enumerate(fin.read().splitlines()):
         potential_part_number = ""
-        local_j = 0
+        start = 0
 
         for j, column in enumerate(row):
             if column.isdigit():
                 if potential_part_number == "":
-                    local_j = j
+                    start = j
                 potential_part_number += column
             else:
                 if column != ".":
-                    symbols.add((i, j, j, column))
+                    symbols.add((i, j, column))
 
                 if potential_part_number != "":
                     numbers.add(
                         (
                             i,
-                            local_j,
-                            local_j + len(potential_part_number) - 1,
+                            start,
+                            start + len(potential_part_number) - 1,
                             potential_part_number,
                         )
                     )
                     potential_part_number = ""
+                    start = 0
+
+        if potential_part_number != "":
+            numbers.add(
+                (
+                    i,
+                    start,
+                    start + len(potential_part_number) - 1,
+                    potential_part_number,
+                )
+            )
 
     # Check if any part numbers are adjacent to symbols
+    sum = 0
+    part2_sum = 0
     for symbol in symbols:
+        # Part 2
+        parts = []
         for part in numbers:
             directions = [
                 (0, 1),
@@ -44,13 +60,23 @@ with open("input.txt") as fin:
                 (-1, -1),
             ]
             for x_offset, y_offset in directions:
-                if symbol[0] + x_offset == part[0] and symbol[1] + y_offset in range(
-                    part[1], part[2] + 1
-                ):
-                    if part not in seen:
-                        seen.add(part)
+                x = symbol[0] + x_offset
+                y = symbol[1] + y_offset
+                if x == part[0] and y >= part[1] and y <= part[2]:
+                    seen.add(part)
+                    parts.append(part)
+
+        # Part 2
+        # Check if the symbol is *
+        parts = list(set(parts))
+        if symbol[2] == "*" and len(parts) == 2:
+            gear_ratio = int(parts[0][3]) * int(parts[1][3])
+            part2_sum += gear_ratio
+
 
 sum = 0
 for part in seen:
     sum += int(part[3])
 print(sum)
+
+print(part2_sum)
