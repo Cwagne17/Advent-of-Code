27626d83853fcd2eb77fe0fc9ebbf21a329d20db
chris wagner <cwagne17@students.towson.edu>
2023-12-03 19:48:20 -0500

    change method

--- 2023/Day_3/main.py
+++ 2023/Day_3/main.py
@@ -1,112 +1,58 @@
-SYMBOLS = ["&", "+", "/", "$", "%", "*", "@", "-", "=", "#"]
+SYMBOLS = ["*", "+", "/", "$", "%", "@", "-", "=", "#"]
 
+numbers = set()
+symbols = set()
 
-def check_for_adjacent_part_numbers(lines, i, j, direction=1):
-    local_j = j
-    part_number = ""
+seen = set()
 
-    while 0 <= local_j < len(lines[i]) and lines[i][local_j].isdigit():
-        part_number = (
-            lines[i][local_j] + part_number
-            if direction == -1
-            else part_number + lines[i][local_j]
-        )
-        local_j += direction
+with open("input.txt") as fin:
+    for i, row in enumerate(fin.read().splitlines()):
+        potential_part_number = ""
+        local_j = 0
 
-    return part_number, local_j - direction
-
-
-def check_left_right(lines, i, j):
-    parts = []
-
-    directions = [
-        (0, 1, 1),  # Right
-        (0, -1, -1),  # Left
-    ]
-
-    for direction in directions:
-        x_offset, y_offset, x_direction = direction
-        part_number, local_j = check_for_adjacent_part_numbers(
-            lines, i + x_offset, j + y_offset, x_direction
-        )
-
-        if part_number:
-            parts.append((i, local_j, part_number))
-
-    return parts
-
-
-part_numbers = set()
-
-with open("input.txt") as f:
-    lines = f.read().splitlines()
-
-    for i, row in enumerate(lines):
         for j, column in enumerate(row):
-            # Check if the current character is a symbol
-            if column in SYMBOLS:
-                # Check if the character left and right of the symbol is a part number
-                parts = check_left_right(lines, i, j)
-                for part in parts:
-                    if part not in part_numbers:
-                        part_numbers.add(part)
+            if column.isdigit():
+                if potential_part_number == "":
+                    local_j = j
+                potential_part_number += column
+            else:
+                if column in SYMBOLS:
+                    symbols.add((i, j, column))
 
-                # Check up
+                if potential_part_number != "":
+                    numbers.add((i, local_j, potential_part_number))
+                    potential_part_number = ""
 
-                upper_right_part_number, local_j1 = check_for_adjacent_part_numbers(
-                    lines, i - 1, j + 1
-                )
-                upper_left_part_number, local_j2 = check_for_adjacent_part_numbers(
-                    lines, i - 1, j - 1, -1
-                )
+    # Check if any part numbers are adjacent to symbols
+    for symbol in symbols:
+        for part in numbers:
+            for iter in range(len(part[2])):
+                directions = [
+                    (0, 1),
+                    (0, -1),
+                    (1, 0),
+                    (-1, 0),
+                    (1, 1),
+                    (1, -1),
+                    (-1, 1),
+                    (-1, -1),
+                ]
+                flag = False
+                for x_offset, y_offset in directions:
+                    for k in range(len(part[2])):
+                        if (
+                            part[0] + x_offset,
+                            part[1] + iter + y_offset,
+                            symbol[2],
+                        ) == symbol:
+                            flag = True
+                            break
 
-                if lines[i - 1][j].isdigit():
-                    part_number = (
-                        upper_left_part_number
-                        + lines[i - 1][j]
-                        + upper_right_part_number
-                    )
-                    part_numbers.add((i - 1, local_j2, part_number))
-                elif (
-                    upper_left_part_number
-                    and (i - 1, local_j2, upper_left_part_number) not in part_numbers
-                ):
-                    part_numbers.add((i - 1, local_j2, upper_left_part_number))
-                elif (
-                    upper_right_part_number
-                    and (i - 1, local_j1, upper_right_part_number) not in part_numbers
-                ):
-                    part_numbers.add((i - 1, local_j1, upper_right_part_number))
+                if flag:
+                    if part not in seen:
+                        seen.add(part)
 
-                # Check down
-
-                lower_right_part_number, local_j1 = check_for_adjacent_part_numbers(
-                    lines, i + 1, j + 1
-                )
-                lower_left_part_number, local_j2 = check_for_adjacent_part_numbers(
-                    lines, i + 1, j - 1, -1
-                )
-
-                if lines[i + 1][j].isdigit():
-                    part_number = (
-                        lower_left_part_number
-                        + lines[i + 1][j]
-                        + lower_right_part_number
-                    )
-                    part_numbers.add((i + 1, local_j2, part_number))
-                elif (
-                    lower_left_part_number
-                    and (i + 1, local_j2, lower_left_part_number) not in part_numbers
-                ):
-                    part_numbers.add((i + 1, local_j2, lower_left_part_number))
-                elif (
-                    lower_right_part_number
-                    and (i + 1, local_j1, lower_right_part_number) not in part_numbers
-                ):
-                    part_numbers.add((i + 1, local_j1, lower_right_part_number))
-
-    sum = 0
-    for i, j, part_number in list(part_numbers):
-        print(part)
-        sum += int(part_number)
-    print(sum)
+sum = 0
+for part in seen:
+    sum += int(part[2])
+print(sum)
